06b22c463d07cbd9cd74fd977674f353b080c839
rafaelb12 <142686443+rafaelb12@users.noreply.github.com>
2026-04-13 13:17:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, send_file
 import os
+import zipfile
 from extractor import processar_pdf
 
 app = Flask(__name__)
-UPLOAD_FOLDER = "uploads"
+UPLOAD_FOLDER = "/tmp"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route("/")
@@ -12,17 +13,26 @@ def index():
 
 @app.route("/upload", methods=["POST"])
 def upload():
-    arquivo = request.files["file"]
+    arquivos = request.files.getlist("files")
 
-    caminho_pdf = os.path.join(UPLOAD_FOLDER, arquivo.filename)
-    arquivo.save(caminho_pdf)
+    caminhos_csv = []
 
-    caminho_csv = processar_pdf(caminho_pdf)
+    for arquivo in arquivos:
+        caminho_pdf = os.path.join(UPLOAD_FOLDER, arquivo.filename)
+        arquivo.save(caminho_pdf)
 
-    return send_file(caminho_csv, as_attachment=True)
+        caminho_csv = processar_pdf(caminho_pdf)
+        caminhos_csv.append(caminho_csv)
 
-import os
+    zip_path = os.path.join(UPLOAD_FOLDER, "resultado.zip")
+
+    with zipfile.ZipFile(zip_path, 'w') as zipf:
+        for caminho in caminhos_csv:
+            zipf.write(caminho, os.path.basename(caminho))
+
+    return send_file(zip_path, as_attachment=True)
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
